887533f8d2d1bf5448588c22c7e957e74eba681c
smhgyf <smhgyf@mail.umkc.edu>
2016-11-10 12:20:18 -0600

    Create Main.cpp

---
+++ Data Structures Project 2B/Main.cpp
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+	string book_title;
+	string customer;
+	string book_ID;
+	string x;
+	string customer_ID;
+	int user_ID;
+	int ISBN;
+	int ID;
+	
+
+	ifstream finBooks("books.txt");
+	ifstream finCustomers("customers.txt");
+	ifstream finRatings("ratings.txt");
+	Bookstore Store;
+	Customer User;
+
+	getline(finBooks, book_title);
+	while (finBooks)
+	{
+		x = finBooks.get();
+		if (finBooks)
+		{
+			if (x == ",")
+			{
+				Store.storebook(ID);
+				book_ID = "";
+			}
+			else
+				book_ID = book_ID + x;
+		}
+		if (x == ",")
+		{
+			x = finBooks.get();
+			getline(finBooks, book_title);
+			Store.storebook(title);
+			system("pause");
+		}
+		
+	}
+	system("pause");
+	getline(finCustomers, customer);
+	while (finCustomers)
+	{
+		x = finCustomers.get();
+		if (finCustomers)
+		{
+			if (x == ",")
+			{
+				Store.storeCustomer(ID);
+				customer_ID = "";
+			}
+			else
+				customer_ID = x;
+		}
+		if (x == ",")
+		{
+			x = finCustomers.get();
+			getline(finCustomers, customer);
+			Store.storebook(customer);
+			system("pause");
+		}
+
+	}
+
+	cout << "Welcome to the Bookstore" << endl;
+	user_ID = cin >> "Please enter your user ID: ";
+	User=Store.Customer_Search(user_ID);
+
+
+
+
+	finBooks.close();
+	finCustomers.close();
+	finRatings.close();
+
+	return 0;
+
+	
+
+}
