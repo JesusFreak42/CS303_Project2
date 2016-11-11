bf00b6d55d0c3842cb2eb44cf2f986ccb9c3fe92
Jacob Quick <jrq5y3@mail.umkc.edu>
2016-11-10 23:29:57 -0600

    Update get_Recommended.h

--- Data Structures Project 2B/get_Recommended.h
+++ Data Structures Project 2B/get_Recommended.h
@@ -1,13 +1,14 @@
-//these are just some experimental functions.  More ideas than anything.  Likely incomplete and probably need tweeking
 
 
 
 
 
-Book vector get_Recommended()
+
+Book vector get_Recommended(Customer current_customer, vector customers) //requires knowledge of customers
+  //contains all the elements and calls the functions
 {
-Books vector uncommon; 
-Customer array[3] best_Customers=find_Highest_Weight( current_customer, vector customers //all the other customers);
+vector <Books> uncommon; 
+vector <Customer> best_Customers=find_Highest_Weight( current_customer, vector customers //all the other customers);
 uncommon=check_three_customers(current_customer,best_Customers[0],best_Customers[1],best_Customers[2]);
 
 Display_Recommended(uncommon,highest_rated);
@@ -15,11 +16,11 @@ Display_Recommended(uncommon,highest_rated);
 }
 
 
-Customer array[3] find_Highest_Weight(Customer current_customer, vector customers //all the other customers)
+vector<Customer> find_Highest_Weight(Customer current_customer, vector customers //all the other customers) 
 {
-  Customer array[3] Weight_List;
+  vector <Customer> Weight_List;
   
-  Weight_List[0]=customers[0];
+  Weight_List[0]=customers[0];     //setting default values to compare to.  the first three customers
   Weight_List[1]=customers[1];
   Weight_List[2]=customers[2];
   int weight_One=weight(current_customer,customers[0]);
@@ -33,7 +34,7 @@ Customer array[3] find_Highest_Weight(Customer current_customer, vector customer
   
   else
   {
-  int compare_Weight=weight(current_customer,customer[i]);
+  int compare_Weight=weight(current_customer,customer[i]);   //if it is in the top three of the vector, it will find it's spot and place it
   if (compare_Weight>weight_Three)
   {
     Weight_List[2]=customer[i];
@@ -57,7 +58,7 @@ Customer array[3] find_Highest_Weight(Customer current_customer, vector customer
   }
 }
 
-int weight(customer cus1, customer cus2)
+int weight(customer cus1, customer cus2)  //compares all the books in common(using parallel programming) and gives weights
 {
 omp_set_num_threads(4);
 
@@ -74,7 +75,7 @@ weight+=compare.(cus1.getbook()[i].getrating(),cus2[j].getbook()[j].getrating())
 return weight;
 }
 
-int compare (num1,num2)
+int compare (num1,num2)  //compares the values, using 5 as a defaualt and temp being the difference in the given ratings
 {
 int temp=0;
 temp=num1-num2
@@ -85,7 +86,7 @@ temp=5+temp;
 return temp;
 }
 
-void find_uncommon(customer cus1, customer cus2, vector& uncommon)
+void find_uncommon(customer cus1, customer cus2, vector& uncommon)  //find any books that they do not have in common to add to recommended books
 {
 bool common=false;
 
@@ -96,7 +97,7 @@ for (int j=0; j<cus2.size();j++)
 {
   if (cus2.book[j].title()==cus1.book[i].title())
   {
-    if (cus2.book[j].rating()>3)
+    if (cus2.book[j].rating()>3)  //make sure it is rated highly before adding to list
     {
       common=true;
     }
@@ -107,7 +108,7 @@ uncommon.pushback(cus2.book[i]);
 }
 
 
-Book vector check_three_customers(original,cus1,cus2,cus3)
+Book vector check_three_customers(original,cus1,cus2,cus3)  //check the three customers for uncommon books, and return the list once 3 are found or all three are checked
 {
 vector uncommon;
 find_uncommon(original,cus1,uncommon)
