117999691a493348e94434c40e4981d4dcae2656
JesusFreak42 <TimothyMeigs@gmail.com>
2016-11-10 21:41:42 -0600

    Update Bookstore.h
    Updates to code, notably regarding the most popular books

--- Data Structures Project 2B/Bookstore.h
+++ Data Structures Project 2B/Bookstore.h
@@ -12,10 +12,22 @@ public:
 	Book search(string title); //Will return Book
 	void rate(Book this_book, Rating rating, Customer user); //MAKE Rating class a child of Book class?
 	void getRecommendations(Customer user); //Will return book
+	int getAverageRating(int bookID); //returns the book's average rating or, if no ratings, -1
+	void updatePopular(Rating newRating);
+
+	Book findBook(int ID) {
+		for (int i = 0; i < books.size(); i++) {
+			if (books[i].getISBN() == ID) return books[i];
+		}
+		Book x;
+		return x;
+	}
 
 private:
 	vector<Book> books;	//Need main function to populate these vectors
 	vector<Customer> customers;
+	vector<Rating> averageRatings; //keeps track of the average rating of each book
 	vector<Book> * popular; //points to the X most popular books in books
+	static int TOTAL_POPULAR;
 };
 
