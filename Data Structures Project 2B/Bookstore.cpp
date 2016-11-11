b23f210d486958f303f7e701042e1e16bd8bd722
JesusFreak42 <TimothyMeigs@gmail.com>
2016-11-10 21:40:52 -0600

    Update Bookstore.cpp
    Added code for the most popular books

--- Data Structures Project 2B/Bookstore.cpp
+++ Data Structures Project 2B/Bookstore.cpp
@@ -14,16 +14,62 @@ void Bookstore::rate(Book this_book, Rating book_rating, Customer user) {
 	bool user_rated = false;
 	if (user.getRating(this_book.getISBN()).getBookID() != -1) {
 		int x = -1;
-		cout << "You have not rated this book before, what would you like to rate it?";
-		cin >> x;
+		cout << "You have not rated this book before, what would you like to rate it? (from 1 to 5)";
+		cin >> x; //reading in the rating from the user
 		book_rating.setRating(x);
+		book_rating.setBookID(this_book.getISBN());
+		user.addRating(book_rating); //adding the rating
+		this_book.setNumRatings(this_book.getNumRatings() + 1); //increasing the total number of ratings for the book
+
+		bool entryExists = false;
+		for (int i = 0; i < averageRatings.size(); i++) { //searches through averageRatings for this book
+			if (averageRatings[i].getBookID() == this_book.getISBN()) { //if we found it
+				averageRatings[i].setRating((averageRatings[i].getRating() + x) / 2); //figures the new average
+				entryExists = true;
+				updatePopular(averageRatings[i]);
+			}
+		}
+		if (entryExists == false) { //if we didn't find the book in averageRatings, an entry doesn't exist so we create one
+			Rating newEntry;
+			newEntry.setBookID(this_book.getISBN());
+			newEntry.setRating(x); //setting up the new rating with the current rating
+			averageRatings.push_back(newEntry); //adding the new entry
+			updatePopular(averageRatings.back());
+		}
 	}
 	else {
 		cout << "You have already rated this book.";
 	}
 }
 
-void getRecommendations(Customer user) { //Will return book
+int Bookstore::getAverageRating(int bookID) {
+	for (int i = 0; i < averageRatings.size(); i++) {
+		if (averageRatings[i].getBookID() == bookID) return averageRatings[i].getRating(); //looking for the book and returning its average rating
+	}
+	return -1; //if no ratings are found
+}
+
+void Bookstore::updatePopular(Rating newRating) {
+	/*vector<Book>::iterator iter;
+	while (iter != popular->end()) {
+		
+	}*/
+
+	for (int i = 0; i < TOTAL_POPULAR; i++) {
+		if (popular->at(i) == NULL) {
+			popular->at(i) = findBook(newRating.getBookID());
+			break;
+		}
+		else if (getAverageRating(popular->at(i).getISBN()) < getAverageRating(newRating.getBookID()) ) {
+			for (int j = TOTAL_POPULAR - 1; j > i; j--) {
+				popular->at(j) = popular->at(j - 1);
+			}
+			popular->at(i) = findBook(newRating.getBookID());
+		}
+	}
+}
+
+void Bookstore::getRecommendations(Customer user) { //Will return book
 
 }
 
